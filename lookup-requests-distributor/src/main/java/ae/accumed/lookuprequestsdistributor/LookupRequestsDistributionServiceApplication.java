a99b120d1d04c819312de1ac84b774d7da6b90a3
Odai Mohammad <omohammad@accumedsy.ae>
2021-06-24 15:00:58 +0300

    Setup infrastructure

---
+++ lookup-requests-distributor/src/main/java/ae/accumed/lookuprequestsdistributor/LookupRequestsDistributionServiceApplication.java
@@ -0,0 +1,13 @@
+package ae.accumed.lookuprequestsdistributor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookupRequestsDistributionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LookupRequestsDistributionServiceApplication.class, args);
+    }
+
+}
---
+++ lookup-requests-distributor/src/test/java/ae/accumed/lookuprequestsdistributor/LookupRequestsDistributionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ae.accumed.lookuprequestsdistributor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookupRequestsDistributionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
